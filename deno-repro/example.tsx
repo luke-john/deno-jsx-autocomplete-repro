348060624a79244959e058b49973bdfedb4ec25d
Luke John <github@lukejohn.me>
2022-07-31 15:41:39 +0800

    create reproduction

---
+++ deno-repro/example.tsx
@@ -0,0 +1,7 @@
+import React from "https://esm.sh/react@18.2.0";
+
+function Example() {
+  // move the cursor to inside the div opening tag
+  // and autocomplete any property.
+  return <div></div>;
+}
